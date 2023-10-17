ffd4a559601e6a3fceff11fc4ae42787856969fe
choiwoohyun123 <90172409+choiwoohyun123@users.noreply.github.com>
2023-10-17 10:07:53 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -6,7 +6,6 @@ def solution(k, dungeons):
     for dungeon_case in permutations(dungeons, len(dungeons)):
         k = k_temp
         result = 0
-        check = True
         for dungeon in dungeon_case:
             if k >= dungeon[0]:
                 k -= dungeon[1]
