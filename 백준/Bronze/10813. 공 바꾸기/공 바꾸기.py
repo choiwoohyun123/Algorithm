c6c27e20a501c79f7e77f4108dbe8d8226d3a685
choiwoohyun123 <90172409+choiwoohyun123@users.noreply.github.com>
2024-01-12 21:35:33 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+bucket = []
+for i in range(1, n + 1):
+  bucket.append(i)
+for _ in range(m):
+  i, j = map(int, input().split())
+  bucket[i-1], bucket[j-1] = bucket[j-1], bucket[i-1]
+  
+print(*bucket)
