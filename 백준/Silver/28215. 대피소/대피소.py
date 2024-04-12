cbd520e018a6c255428135b53b2146fb4e9cc05f
choiwoohyun123 <90172409+choiwoohyun123@users.noreply.github.com>
2024-04-12 23:53:49 +0900

    [Silver IV] Title: 대피소, Time: 1276 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.py
+++ 백준/Silver/28215. 대피소/대피소.py
@@ -1,38 +1,21 @@
 from itertools import combinations
 import sys
 
-input = sys.stdin.readline
+input = sys.stdin.read
+data = input().split()
 
-n, k = map(int, input().split())
-
-co_li = []
-index_li = []
+n, k = int(data[0]), int(data[1])
+co_li = [(int(data[i*2+2]), int(data[i*2+3])) for i in range(n)]
 
 answer = float('inf')
 
-for _ in range(n):
-  co_li.append(list(map(int, input().split())))
-  
-for i in range(n):
-  index_li.append(i)
-  
 for shelters in combinations(co_li, k):
-  houses = []
-  distances = []
-  for co in co_li:
-    if co not in shelters:
-      houses.append(co)
-  for house in houses:
-    shel_di = float('inf')
-    for shelter in shelters:
-      temp = abs(shelter[0] - house[0]) + abs(shelter[1] - house[1])
-      shel_di = min(temp, shel_di)
-    
-    distances.append(shel_di)
-  
-  answer = min(max(distances), answer)
-      
-  
+    max_min_dist = 0
+    for house in co_li:
+        if house in shelters:
+            continue
+        min_dist = min(abs(shelter[0] - house[0]) + abs(shelter[1] - house[1]) for shelter in shelters)
+        max_min_dist = max(max_min_dist, min_dist)
+    answer = min(answer, max_min_dist)
+
 print(answer)
-  
-    
